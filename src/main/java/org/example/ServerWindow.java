e38776ef98a7395340b4d44b0d03c4b6b27a6185
Chernykh Ivan <114511742+Oelob@users.noreply.github.com>
2024-07-19 09:15:36 +0500

    Создал окно сервера

---
+++ src/main/java/org/example/ServerWindow.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ServerWindow extends JFrame {
+    private static final int POS_X = 500;
+    private static final int POS_Y = 550;
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+
+    private final JButton btnStart = new JButton("Start");
+    private final JButton btnStop = new JButton("Stop");
+    private final JTextArea log = new JTextArea();
+    private boolean isServerWorking;
+
+    public ServerWindow(){
+        isServerWorking = false;
+        btnStop.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isServerWorking = false;
+                System.out.println("Server stoped " + isServerWorking + "\n");
+            }
+        });
+
+        btnStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isServerWorking = true;
+                System.out.println("Server started " + isServerWorking + "\n");
+            }
+        });
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setBounds(POS_X, POS_Y, WIDTH, HEIGHT);
+        setTitle("Chat server");
+        setAlwaysOnTop(true);
+        setLayout(new GridLayout(1,2));
+        add(btnStart);
+        add(btnStop);
+
+        setVisible(true);
+    }
+}
